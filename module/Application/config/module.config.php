da44d2f3e921b3948aa4ee3d38b4552509991732
Steve Rayner <srayner02@gmail.com>
2016-04-09 21:38:37 +0100

    Added doctrine dependency.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -99,4 +99,21 @@ return array(
             ),
         ),
     ),
+    
+    // Doctrine config
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                ),
+            ),
+        ),
+    ),
+    
 );
